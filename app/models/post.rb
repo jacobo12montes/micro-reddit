b2448a83ca2f595bc781cff1d6ca1894fa2c9604
Juan <jacobo12.montes@gmail.com>
2020-09-25 11:34:36 -0500

    Add the model for the comment object

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, :url, presence: true
   URL_REGEXP = URI.regexp
   validates :url, format: { with: URL_REGEXP, message: 'You provided an invalid URL' }
-
   belongs_to :user
+  has_many :comments
 end
