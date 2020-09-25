0791d12892b355d7598f157845d8c2e11fc1505f
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-09-25 18:52:17 +0300

    Add posts validations and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, :url, presence: true
+  URL_REGEXP = URI.regexp
+  validates :url, format: { with: URL_REGEXP, message: 'You provided an invalid URL' }
+
+  belongs_to :user
 end
