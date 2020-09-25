316f1b81615f2355e08660baa5bedae1f60af36d
Juan <jacobo12.montes@gmail.com>
2020-09-25 11:56:36 -0500

    Add the files for the linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :url, presence: true
-  URL_REGEXP = URI.regexp
+  URL_REGEXP = URI::DEFAULT_PARSER.make_regexp
   validates :url, format: { with: URL_REGEXP, message: 'You provided an invalid URL' }
   belongs_to :user
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: { in: 4..10 }
-    validates :password, presence: true, length: { in: 6..20 }
-    has_many :comments
-    has_many :posts
+  validates :username, presence: true, uniqueness: true, length: { in: 4..10 }
+  validates :password, presence: true, length: { in: 6..20 }
+  has_many :comments
+  has_many :posts
 end
